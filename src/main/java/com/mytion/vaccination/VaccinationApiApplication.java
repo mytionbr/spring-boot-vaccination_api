689c0fbe6631ccc5c4427901cf3c6015ca0eede4
mytionBr <matheuss.jesus2017@gmail.com>
2021-05-23 17:59:52 -0300

    initial commit

---
+++ src/main/java/com/mytion/vaccination/VaccinationApiApplication.java
@@ -0,0 +1,13 @@
+package com.mytion.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytion/vaccination/VaccinationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytion.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
